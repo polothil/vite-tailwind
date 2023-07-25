6bdd08d55a027a9b35d845f0206a851e4e07528b
aravindpvk <aravindpvk@gmail.com>
2023-07-25 21:45:08 +0200

    random changes

--- src/App.jsx
+++ src/App.jsx
@@ -12,7 +12,7 @@ function App() {
     <div>
       <Navbar />
       <Routes>
-        <Route exact path='/vite-tailwind/' element={<Home />} />
+        <Route exact path='/vite-tailwind' element={<Home />} />
         <Route path='/vite-tailwind/services' element={<Services />} />
         <Route path='/vite-tailwind/about' element={<AboutUs />} />
         <Route path='/vite-tailwind/contact' element={<Contact />} />
