c91a26e5c06a8aa675468eb7e0bfdb9ad0922aa9
aravindpvk <aravindpvk@gmail.com>
2023-07-25 15:20:06 +0200

    Dummy pages added

--- src/App.jsx
+++ src/App.jsx
@@ -1,11 +1,11 @@
-import { BrowserRouter as Router, Routes, Route } from "react-router-dom";
-import Navbar from "./components/header/Navbar";
-import Home from "./pages/Home/Home";
-import Services from "./pages/Services/Services";
-import Portfolio from "./pages/Portfolio/Portfolio";
-import Contact from "./pages/Contact/Contact";
-import Footer from "./components/footer/Footer";
-import "./App.css";
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
+import Navbar from './components/header/Navbar';
+import Home from './pages/Home/Home';
+import Services from './pages/Services/Services';
+import AboutUs from './pages/AboutUs/AboutUs';
+import Contact from './pages/Contact/Contact';
+import Footer from './components/footer/Footer';
+import './App.css';
 
 function App() {
   return (
@@ -13,10 +13,10 @@ function App() {
       <Router>
         <Navbar />
         <Routes>
-          <Route exact path="/" element={<Home />} />
-          <Route path="/services" element={<Services />} />
-          <Route path="/portfolio" element={<Portfolio />} />
-          <Route path="/contact" element={<Contact />} />
+          <Route exact path='/' element={<Home />} />
+          <Route path='/services' element={<Services />} />
+          <Route path='/about' element={<AboutUs />} />
+          <Route path='/contact' element={<Contact />} />
         </Routes>
         <Footer />
       </Router>
